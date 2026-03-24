f33092eade9e3aa756719a822843c4e465ca0bb8
Егор Иванов <alex09090909app.p@gmail.com>
2026-03-24 23:00:52 +0300

    New Accounts (client)

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -63,7 +63,6 @@ const Shop = observer(() => {
 
     const showCategories = product.selectedType?.id === 1 || product.selectedType?.id === 4;
     const showPlatforms = product.selectedType?.id === 2;
-    const showGames = product.selectedType?.id === 3;
     const showAll = !product.selectedType;
 
     return (

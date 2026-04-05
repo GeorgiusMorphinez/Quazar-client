e332e3c67294bbc3c6b0d387a3dfe0e34af89cae
Егор Иванов <alex09090909app.p@gmail.com>
2026-04-05 17:46:46 +0300

    CSS (client)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import GameStore from "./store/GameStore";
 import BasketStore from "./store/BasketStore";
 import ProductStore from "./store/ProductStore";
 import OrderStore from "./store/OrderStore"; // Новый
-import './global';
+import 'C:\Users\alex0\WebstormProjects\NewGameStore\client\src\global';
+
 
 export const Context = createContext(null);
 
